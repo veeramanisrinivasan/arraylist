01286dcf23502d2d35daf4087b320beb21997746
Janagiram <veeramani.ram1994@gmail.com>
2015-07-29 10:58:52 +0530

    Intial start off the program

---
+++ array list/src/array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class array {
+	public static void main(String args[])
+	{
+		ArrayList a1=new ArrayList();
+		System.out.println("intial size="+a1.size());
+		a1.add("c");
+		a1.add("d");
+		a1.add("e");
+		a1.add("f");
+		a1.add("g");
+		System.out.println("after addition="+a1.size());
+		System.out.println("content="+a1);
+		a1.remove("f");
+		System.out.println("after deletion="+a1);
+		
+	}
+}
